2d5dadd24a811d7227d09a745aec995b5eab8ca0
avaldinger <adam.valdinger@gmail.com>
2021-03-04 00:42:15 +0100

    Models created, admin interface edited

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from .models import User, Post, Comment, Like, Follow
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "creationDate", "post", "likes")
+    # list_editable = ("item", "description", "category", "sold", "price", "image", "bidWinner", "createdBy")
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("id", "likedBy", "post", "liked", "timestamp")
+    # list_editable = ("currentBid", "nextBid")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "comment", "username", "post", "timestamp")
+    # list_editable = ("comment",)
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "followedBy", "timestamp")
+
+
+
+admin.site.register(User)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Follow, FollowAdmin) 
--- network/models.py
+++ network/models.py
@@ -3,7 +3,78 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    profilePicture = models.CharField(blank=True, max_length=255)
+    followers = models.IntegerField(default=0)
 
-class Post():
+class Post(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    creationDate = models.DateTimeField(auto_now=True)
+    post = models.TextField(blank=True)
+    image = models.CharField(blank=True, max_length=255)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Post id: {self.id}; created by: {self.username}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "creationDate": self.creationDate.strftime("%b %d %Y, %I:%M %p"),
+            "post": self.post,
+            "image": self.image,
+            "likes": self.likes
+        }
+
+class Like(models.Model):
+    likedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, default=None, on_delete=models.CASCADE)
+    liked = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Like id: {self.id}; Liked by: {self.likedBy}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "likedBy": self.likedBy,
+            "liked": self.liked,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")
+        }
+
+class Comment(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, )
+    post =  models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    comment = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment id: {self.id}; Commented by: {self.username} at {self.timestamp}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "post": self.post,
+            "comment": self.comment,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")
+        }
+
+class Follow(models.Model):
+    user = models.IntegerField()
+    followedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} followed by: {self.followedBy} at {self.timestamp}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user,
+            "followedBy": self.followedBy,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")
+        }
+
     
